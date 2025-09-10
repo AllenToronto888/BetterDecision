6b779fcb508fb8682b52bd381b97bf2b600ad956
AllenToronto888 <allen.toronto@outlook.com>
2025-09-10 00:14:03 -0400

    fix Hermes warning

--- metro.config.js
+++ metro.config.js
@@ -2,4 +2,17 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+// Configure Hermes to suppress warnings that cause build failures
+config.transformer = {
+  ...config.transformer,
+  minifierConfig: {
+    keep_fargs: true,
+    mangle: {
+      keep_fnames: true,
+    },
+    // Suppress warnings that don't affect functionality
+    warnings: false,
+  },
+};
+
 module.exports = config;
