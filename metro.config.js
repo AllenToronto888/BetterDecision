1a2bacfa440c49858b12ef7bec8b7fa172961d77
AllenToronto888 <allen.toronto@outlook.com>
2025-09-10 03:46:21 -0400

    increase bulid number

--- metro.config.js
+++ metro.config.js
@@ -2,7 +2,9 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-// Disable Hermes entirely to avoid warning-based build failures
+// TODO: Re-enable Hermes in future for better performance (2-3x faster startup, 30% less memory)
+// Currently disabled to avoid build issues - can be enabled later when needed
+// To enable: remove this transformer override and set "jsEngine": "hermes" in app.json
 config.transformer = {
   ...config.transformer,
   // Completely disable Hermes for now
